81dde590bdf5f0d7b11d479805d1c3e52f0be821
Ja <ja@Jas-MacBook-Pro.local>
2017-12-13 22:52:39 -0500

    dig

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// generate a random number for the computer to use
+var cpuNumber = Math.floor((Math.random()*120) + 19);
+// The crystals need to generate a random number also
+var crystalNumber = Math.floor((Math.random()* 12)+ 1);
+// The score of the random number needs to be stored
+var score = [];
+// The wins and losses need to be stored;
+var wins = "";
+var losses = "";
